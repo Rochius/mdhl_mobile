ea2a2e22fca698d751fb62a08190562bc9cf9169
Rocio Morales <roncio.93@gmail.com>
2023-01-26 04:30:04 -0300

    :art: Styles added

--- js/shortcut_btn.js
+++ js/shortcut_btn.js
@@ -1,2 +1,14 @@
-const shortcutBtn = document.querySelector(".shortcuts_title")
-const shortcutList = document.querySelector(".shortcuts_list")
+const addEventShortcut = ()=>{
+    const shortcutBtn = document.querySelector(".shortcuts_title")
+    const shortcutList = document.querySelector(".shortcuts_list")
+
+    shortcutBtn.addEventListener('click', ()=>{
+        if(shortcutList.classList.contains("hide")){
+            shortcutList.classList.remove("hide")
+        }else{
+            shortcutList.classList.add("hide")
+        }
+    })
+}
+
+addEventShortcut()
